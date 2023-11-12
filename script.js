182611b3b700855d324e3b09cb8f2fd808db2cf3
AdventCal <150554263+AdventCal@users.noreply.github.com>
2023-11-12 12:12:30 -0700

    Update script.js
    Fixed syntax error

--- script.js
+++ script.js
@@ -5,70 +5,70 @@ function checkCode() {
     if (enteredCode.toLowerCase() === 'santa2023') {
         resultParagraph.textContent = 'You have discovered an Easter Egg! <- We know it is Christmas';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'snowflakes') {
+    } else if (enteredCode.toLowerCase() === 'snowflakes') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'christmas tree') {
+    } else if (enteredCode.toLowerCase() === 'christmas tree') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'ice skates') {
+    } else if (enteredCode.toLowerCase() === 'ice skates') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'icicles') {
+    } else if (enteredCode.toLowerCase() === 'icicles') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'snowman') {
+    } else if (enteredCode.toLowerCase() === 'snowman') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'ice') {
+    } else if (enteredCode.toLowerCase() === 'ice') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'christmas present') {
+    } else if (enteredCode.toLowerCase() === 'christmas present') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'christmas ornaments') {
+    } else if (enteredCode.toLowerCase() === 'christmas ornaments') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'wind') {
+    } else if (enteredCode.toLowerCase() === 'wind') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'gingerbread cookie') {
+    } else if (enteredCode.toLowerCase() === 'gingerbread cookie') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'december') {
+    } else if (enteredCode.toLowerCase() === 'december') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'poinsettia') {
+    } else if (enteredCode.toLowerCase() === 'poinsettia') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'ice rink') {
+    } else if (enteredCode.toLowerCase() === 'ice rink') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'scarf') {
+    } else if (enteredCode.toLowerCase() === 'scarf') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'christmas carol') {
+    } else if (enteredCode.toLowerCase() === 'christmas carol') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'sledding') {
+    } else if (enteredCode.toLowerCase() === 'sledding') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'ice castle') {
+    } else if (enteredCode.toLowerCase() === 'ice castle') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'penguin') {
+    } else if (enteredCode.toLowerCase() === 'penguin') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'northern lights') {
+    } else if (enteredCode.toLowerCase() === 'northern lights') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'winter solstice') {
+    } else if (enteredCode.toLowerCase() === 'winter solstice') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'orion') {
+    } else if (enteredCode.toLowerCase() === 'orion') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
-    } elseif (enteredCode.toLowerCase() === 'ice sculpting') {
+    } else if (enteredCode.toLowerCase() === 'ice sculpting') {
         resultParagraph.textContent = 'The activity for today is: !';
         resultParagraph.style.color = '#218838'; // Darker Green
     } else {
